53a40877924a0e2d27f1e83f7ef3c1eda0c4d619
Teddy Kalp <tkalp@siacharts.com>
2024-01-13 09:45:38 -0700

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    # validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    # validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
+    def say_hello 
+        puts "Hello World"
+    end
+end
